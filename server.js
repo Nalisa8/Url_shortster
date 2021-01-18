0a6a61f433b1b18ab889847fe670036843c3d984
Nalisa8 <nat.galbano@gmail.com>
2021-01-18 11:22:07 +0000

    create database connection mongoose

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    userNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.set('view engine','ejs')
+
 app.get('/',(req,res)=>{
 res.render('index')
+})
+
+app.post('/shortUrls',(req, res)=>{
+
 })
 app.listen(process.env.PORT || 5000);
